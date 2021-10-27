ec01d44a54ff1b9df375ac430d2b61f11b4b750c
Samandar Turdiev <firsturdiev@gmail.com>
2021-10-27 07:12:26 +0000

    dd

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,32 +1,50 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./AddTodo.css";
 
-const AddTodo = ({todos, setTodos}) => {
+const AddTodo = ({ todos, setTodos }) => {
   const inputRef = useRef();
+  const [disabled, setDisabled] = useState(false);
+  const [loading, setLoading] = useState('Add todo');
+
   const handleFormSubmit = (evt) => {
     evt.preventDefault();
 
     if (inputRef.current.value.trim()) {
-      setTodos([
-        ...todos, 
-        {
-          id: todos.length,
+      setDisabled(true);
+      setLoading('Adding...')
+
+      fetch('https://jsonplaceholder.typicode.com/todos/', {
+        method: 'POST',
+        body: JSON.stringify({
+          uniqueId: Math.floor(Math.random() * 100),
           name: inputRef.current.value,
           completed: false
-        }
-      ]);
-
-      inputRef.current.value = null;
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then(response => response.json())
+        .then(newTodo => {
+          setTodos(todos => ([
+            newTodo,
+            ...todos
+          ])
+          )
+        })
+        .then(() => {
+          setLoading('Add todo');
+          setDisabled(false);
+        })
     }
-    // Name, Description, Tags (<- 1ta input) (umumiy 3ta input)
-  };
+  }
 
   return (
     <form onSubmit={handleFormSubmit} className="add-todo" action="#">
       <input ref={inputRef} placeholder="Todo name" className="add-todo__input" type="text" />
-      <button className="add-todo__submit" type="submit" >Add todo</button>
+      <button disabled={disabled} className="add-todo__submit" type="submit">{loading}</button>
     </form>
   );
-};
+}
 
 export default AddTodo;
--- src/components/TodosList/TodosList.js
+++ src/components/TodosList/TodosList.js
@@ -2,12 +2,10 @@ import TodoItem from "../TodoItem/TodoItem";
 import "./TodosList.css";
 
 const TodosList = ({ todos, setTodos }) => {
-  // localStorage.setItem('todos', JSON.stringify(todos));
-  
   return (
     <ul className="todos-list">
       {
-        todos.map((todo) => <TodoItem todos={todos} setTodos={setTodos} {...todo} key={todo.id} />)
+        todos.map((todo) => <TodoItem todos={todos} setTodos={setTodos} {...todo} key={todo.uniqueId} />)
       }
     </ul>
   );
